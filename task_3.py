082478e644b730490cb94528fe0e837857f472e9
mismailova <meryem.ismailova@yandex.ru>
2026-03-15 11:40:27 +0500

    Исправлены определение переменной points, вызов метода в дочернем классе, удален конструктор, добавлен возврат points во всех условиях

--- task_3.py
+++ task_3.py
@@ -1,39 +1,34 @@
-points = 0
-
 class PointsForPlace:
-    def __init__(self):
-        pass
 
     @staticmethod
     def get_points_for_place(place):
+        points = 0
         if place > 100:
             print('Баллы начисляются только первым 100 участникам')
+            return points
         elif place < 1:
             print('Спортсмен не может занять нулевое или отрицательное место')
+            return points
         else:
             points = 101 - place
             return points
 
 class PointsForMeters:
-    def __init__(self):
-        pass
 
     @staticmethod
     def get_points_for_meters(meters):
+        points = 0
         if meters < 0:
             print('Количество метров не может быть отрицательным')
+            return points
         else:
             points = meters * 0.5
             return points
 
 class TotalPoints(PointsForPlace, PointsForMeters):
-    def __init__(self):
-        PointsForPlace.__init__(self)
-        PointsForMeters.__init__(self)
 
-    @staticmethod
-    def get_total_points(place, meters):
-        total = PointsForPlace.get_points_for_place(place) + PointsForMeters.get_points_for_meters(meters)
+    def get_total_points(self,place, meters):
+        total = self.get_points_for_place(place) + self.get_points_for_meters(meters)
         return total
 
 points_for_place = PointsForPlace()
